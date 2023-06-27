44f61a1e1d6775a4784788469bf4a547ba40148b
MikeCode11 <98663246+MikeCode11@users.noreply.github.com>
2023-06-26 19:08:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -20,8 +20,6 @@ function my_touchstart(e) {
 
   last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
   last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
-
-  // Dibujar el anillo en la posición inicial
   ctx.beginPath();
   ctx.strokeStyle = color;
   ctx.lineWidth = width_of_line;
@@ -31,7 +29,6 @@ function my_touchstart(e) {
 
 canvas.addEventListener("touchmove", my_touchmove);
 function my_touchmove(e) {
-  // Evitar que se dibujen líneas durante el movimiento del toque
   e.preventDefault();
 }
 
@@ -43,7 +40,6 @@ function my_mousedown(e) {
   last_position_of_x = e.clientX - canvas.offsetLeft;
   last_position_of_y = e.clientY - canvas.offsetTop;
 
-  // Dibujar el anillo en la posición inicial
   ctx.beginPath();
   ctx.strokeStyle = color;
   ctx.lineWidth = width_of_line;
@@ -53,7 +49,6 @@ function my_mousedown(e) {
 
 canvas.addEventListener("mousemove", my_mousemove);
 function my_mousemove(e) {
-  // Evitar que se dibujen líneas durante el movimiento del mouse
   if (e.buttons !== 1) {
     return;
   }
@@ -61,7 +56,6 @@ function my_mousemove(e) {
   var current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
   var current_position_of_mouse_y = e.clientY - canvas.offsetTop;
 
-  // Dibujar el anillo en la posición actual del mouse
   ctx.beginPath();
   ctx.strokeStyle = color;
   ctx.lineWidth = width_of_line;
@@ -74,12 +68,10 @@ function my_mousemove(e) {
 
 canvas.addEventListener("mouseup", my_mouseup);
 function my_mouseup(e) {
-  // No se requiere ninguna acción específica al soltar el mouse
 }
 
 canvas.addEventListener("mouseleave", my_mouseleave);
 function my_mouseleave(e) {
-  // No se requiere ninguna acción específica al salir del lienzo
 }
 
 function ClearArea() {
